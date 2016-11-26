6c0fb28bc8b336f008f67a946d13ea2359e50f08
wjt <jtwang.me@gmail.com>
2016-11-26 15:54:16 +0800

    initial commit

---
+++ src/main/java/Resolver.java
@@ -0,0 +1,26 @@
+import io.bretty.solver.normalization.*;
+
+import java.util.Set;
+
+public class Resolver {
+    public static void main(String[] args) {
+        Set<Attribute> attrs = Attribute.getSet("employee_id, employee_bonus_ratio, teacher_id, manager_id, course_id, mandatory_status, department_id, exam_grade, exam_date, make_up_exam_app_date, make_up_exam_app_approval_status, make_up_exam_app_fee, make_up_exam_grade");
+        Set<FuncDep> fds = FuncDep.getSet("manager_id --> department_id; department_id --> manager_id; course_id --> teacher_id; employee_id, course_id --> exam_grade, exam_date; course_id, department_id --> mandatory_status; employee_id --> department_id; exam_date, make_up_exam_app_date --> make_up_exam_app_fee");
+        Relation relation = new Relation(attrs, fds);
+        Set<Relation> r3nf = relation.decomposeTo3NF();
+        for (Relation r:
+                r3nf) {
+            System.out.println(r);
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
+        Set<Relation> rbcnf = relation.decomposeToBCNF();
+        for (Relation r:
+                rbcnf) {
+            System.out.println(r);
+            System.out.println();
+        }
+    }
+}
